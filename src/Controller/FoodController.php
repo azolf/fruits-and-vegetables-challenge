fd1cfc239a9530ea1a375d8ecf1d181fdf32c90f
Amirhosein Zolfaghari <amirhosein.zlf@gmail.com>
2025-07-04 17:26:27 +0200

    fixed format and added new route to create

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\DTO\FoodFilter;
 use App\Repository\FoodRepository;
 use App\Repository\FruitRepository;
+use App\Request\Food\CreateRequest;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,15 +18,32 @@ use App\Service\FoodService;
 
 class FoodController extends AbstractController
 {
-    #[Route('/foods', name: 'list_foods', methods: ['GET', 'HEAD'])]
-    public function index(Request $request,
-        FoodService $foodService,
-        SerializerInterface $serializer
-        ): JsonResponse
-    {        
-        $filters = $request->query->all('filters');
-        $result = $foodService->getFilteredFoods($filters);
-        $jsonContent  = $serializer->serialize($result, format: 'json');        
-        return JsonResponse::fromJsonString($jsonContent);
+  #[Route('/foods', name: 'list_foods', methods: ['GET', 'HEAD'])]
+  public function index(
+    Request $request,
+    FoodService $foodService,
+    SerializerInterface $serializer
+  ): JsonResponse {
+    $filters = $request->query->all('filters');
+    $result = $foodService->getFilteredFoods($filters);
+    $jsonContent = $serializer->serialize($result, format: 'json');
+    return JsonResponse::fromJsonString($jsonContent);
+  }
+
+  #[Route('/foods', name: 'create_food', methods: ['POST'])]
+  public function create(
+    CreateRequest $request,
+    FoodService $foodService,
+    SerializerInterface $serializer
+  ): JsonResponse {
+
+    $errors = $request->validate();
+    if (count($errors['errors']) > 0) {
+      return JsonResponse::fromJsonString(json_encode($errors));
     }
+
+    $result = $foodService->create($request->toArray());
+    $jsonContent = $serializer->serialize($result, format: 'json');
+    return JsonResponse::fromJsonString($jsonContent);
+  }
 }
