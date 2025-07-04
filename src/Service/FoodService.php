6a0a77f143afe358b067ca90fa99c8ed3749ba52
Amirhosein Zolfaghari <amirhosein.zlf@gmail.com>
2025-07-04 11:47:20 +0200

    added food service to process array of incoming foods

---
+++ src/Service/FoodService.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Service;
+
+use App\Factory\FoodFactory;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FoodService
+{
+  private $foodFactory;
+  private $entityManager;
+
+  public function __construct(FoodFactory $foodFactory, EntityManagerInterface $entityManager)
+  {
+    $this->foodFactory = $foodFactory;
+    $this->entityManager = $entityManager;
+  }
+  public function processRequest(array $data): array
+  {
+    $result = [];
+    foreach ($data as $item) {
+      $food = $this->foodFactory->create($item['type']);
+      $food->setName($item['name'])
+        ->setQuantity($item['quantity'], $item['unit']);
+      $this->entityManager->persist($food);
+      $result[] = $food;
+    }
+    
+    return $result;
+  }
+}
