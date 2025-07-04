8892b94281dc5ad3a6020684fc528863f66bb5aa
Amirhosein Zolfaghari <amirhosein.zlf@gmail.com>
2025-07-04 18:12:34 +0200

    cleanup and removed unused codes

--- src/Service/FoodService.php
+++ src/Service/FoodService.php
@@ -3,12 +3,9 @@ namespace App\Service;
 
 use App\DTO\FoodFilter;
 use App\Entity\Food;
-use App\Entity\Fruit;
-use App\Entity\Vegetable;
 use App\Factory\FoodFactory;
 use App\Repository\FoodRepository;
-use App\Repository\FruitRepository;
-use App\Repository\VegetableRepository;
+
 
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -19,10 +16,11 @@ class FoodService
 
   private $entityManager;
 
-  public function __construct(FoodFactory $foodFactory,
+  public function __construct(
+    FoodFactory $foodFactory,
     FoodRepository $foodRepository,
-    EntityManagerInterface $entityManager)
-  {
+    EntityManagerInterface $entityManager
+  ) {
     $this->foodFactory = $foodFactory;
     $this->foodRepository = $foodRepository;
     $this->entityManager = $entityManager;
@@ -36,21 +34,5 @@ class FoodService
   public function create(array $data): Food
   {
     return $this->foodRepository->addFromArray($data);
-  }
-
-  public function processRequest(array $data): array
-  {
-    foreach ($data as $item) {
-      $food = $this->foodFactory->create($item['type']);
-      $food->setName($item['name'])
-        ->setQuantity($item['quantity'], $item['unit']);
-      $this->entityManager->persist($food);
-    }
-    $this->entityManager->flush();
-
-    return [
-      'fruits' => $this->fruitRepository,
-      'vegetables' => $this->vegetableRepository,
-    ];
   }
 }
