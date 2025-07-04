88e04a71e3d855975fdbcf64b36c40d88883d11c
Amirhosein Zolfaghari <amirhosein.zlf@gmail.com>
2025-07-04 12:09:37 +0200

    return repositories in the service

--- src/Service/FoodService.php
+++ src/Service/FoodService.php
@@ -1,7 +1,11 @@
 <?php
 namespace App\Service;
 
+use App\Entity\Fruit;
+use App\Entity\Vegetable;
 use App\Factory\FoodFactory;
+use App\Repository\FruitRepository;
+use App\Repository\VegetableRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class FoodService
@@ -9,23 +13,33 @@ class FoodService
   private $foodFactory;
   private $entityManager;
 
-  public function __construct(FoodFactory $foodFactory, EntityManagerInterface $entityManager)
+  private $vegetableRepository;
+
+  private $fruitRepository;
+
+  public function __construct(FoodFactory $foodFactory,
+  EntityManagerInterface $entityManager,
+  FruitRepository $fruitRepository,
+  VegetableRepository $vegetableRepository)
   {
     $this->foodFactory = $foodFactory;
     $this->entityManager = $entityManager;
+    $this->fruitRepository = $fruitRepository;
+    $this->vegetableRepository = $vegetableRepository;
   }
   public function processRequest(array $data): array
   {
-    $result = [];
     foreach ($data as $item) {
       $food = $this->foodFactory->create($item['type']);
       $food->setName($item['name'])
         ->setQuantity($item['quantity'], $item['unit']);
       $this->entityManager->persist($food);
-      $result[] = $food;
     }
     $this->entityManager->flush();
-    
-    return $result;
+
+    return [
+      'fruits' => $this->fruitRepository,
+      'vegetables' => $this->vegetableRepository,
+    ];
   }
 }
